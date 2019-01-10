d1eb9d8937c156a1148e696c99208679ee77f162
John Malloy <jfmalloy1@gmail.com>
2019-01-10 18:40:34 +0000

    Random seeds - 5, weighted

--- rseeds.py
+++ rseeds.py
@@ -7,21 +7,42 @@ seed_set = []
 
 #Running sum of concentrations
 rsums = np.cumsum(compounds['Concentration']).tolist()
+print(rsums)
 
 #path to csv file
-path = "rseeds.dat"
+path = "rseeds_5w.dat"
 f = open(path, "w")
 
-#create 100 sets of random seed sets, each 10 compounds long
+#create 100 sets of random seed sets
 for i in range(100):
     seed_set = []
-    #Calculate 10 random numbers, find compound associated with each
-    while len(seed_set) < 10:
+
+    #VERSION: Calculate 10 random numbers weighted average
+    # while len(seed_set) < 10:
+    #     r = random.uniform(0, np.sum(compounds['Concentration']))
+    #     #print(r)
+    #     gc = np.extract(rsums > r, rsums)
+    #     element = rsums.index(gc[0])
+    #     if (compounds['Compound'][element] not in seed_set):
+    #         seed_set.append(compounds['Compound'][element])
+
+    #VERSION: 5 random (with weights)
+    while len(seed_set) < 5:
         r = random.uniform(0, np.sum(compounds['Concentration']))
         gc = np.extract(rsums > r, rsums)
         element = rsums.index(gc[0])
         if (compounds['Compound'][element] not in seed_set):
             seed_set.append(compounds['Compound'][element])
+
+    #VERSION: 5 random (no weight)
+    # r = compounds.sample(n=5)
+    # seed_set = r['Compound'].tolist()
+    # print(seed_set)
+
+    #VERSION: 10 random (no weight)
+    # r = compounds.sample(n=10)
+    # seed_set = r['Compound'].tolist()
+    # print(seed_set)
 
     #Final seet set outputting to a file
     for seed in seed_set:
