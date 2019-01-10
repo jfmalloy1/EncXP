d65ad6790cbc4f31df35c3e97e8159ff87cf18f1
John Malloy <jfmalloy1@gmail.com>
2019-01-10 20:24:50 +0000

    Further addition to updated randomization

--- rseeds.py
+++ rseeds.py
@@ -10,29 +10,30 @@ rsums = np.cumsum(compounds['Concentration']).tolist()
 print(rsums)
 
 #path to csv file
-path = "rseeds_5w.dat"
+path = "rseeds_10w.dat"
 f = open(path, "w")
 
 #create 100 sets of random seed sets
 for i in range(100):
+    print(i)
     seed_set = []
-
     #VERSION: Calculate 10 random numbers weighted average
-    # while len(seed_set) < 10:
-    #     r = random.uniform(0, np.sum(compounds['Concentration']))
-    #     #print(r)
-    #     gc = np.extract(rsums > r, rsums)
-    #     element = rsums.index(gc[0])
-    #     if (compounds['Compound'][element] not in seed_set):
-    #         seed_set.append(compounds['Compound'][element])
-
-    #VERSION: 5 random (with weights)
-    while len(seed_set) < 5:
+    while len(seed_set) < 10:
         r = random.uniform(0, np.sum(compounds['Concentration']))
+        #print(r)
         gc = np.extract(rsums > r, rsums)
         element = rsums.index(gc[0])
         if (compounds['Compound'][element] not in seed_set):
             seed_set.append(compounds['Compound'][element])
+            print(seed_set)
+
+    #VERSION: 5 random (with weights)
+    # while len(seed_set) < 5:
+    #     r = random.uniform(0, np.sum(compounds['Concentration']))
+    #     gc = np.extract(rsums > r, rsums)
+    #     element = rsums.index(gc[0])
+    #     if (compounds['Compound'][element] not in seed_set):
+    #         seed_set.append(compounds['Compound'][element])
 
     #VERSION: 5 random (no weight)
     # r = compounds.sample(n=5)
