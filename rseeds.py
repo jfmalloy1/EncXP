2b5fd3a9b8c3eefeb6399e3d30b70109e77962bf
John Malloy <jfmalloy1@gmail.com>
2019-07-09 09:40:52 -0700

    Random sampling updates

--- rseeds.py
+++ rseeds.py
@@ -10,7 +10,7 @@ rsums = np.cumsum(compounds['Concentration']).tolist()
 print(rsums)
 
 #path to csv file
-path = "rseeds_10w.dat"
+path = "rseeds_5w.dat"
 f = open(path, "w")
 
 #create 100 sets of random seed sets
@@ -18,31 +18,29 @@ for i in range(100):
     print(i)
     seed_set = []
     #VERSION: Calculate 10 random numbers weighted average
-    while len(seed_set) < 10:
-        r = random.uniform(0, np.sum(compounds['Concentration']))
-        #print(r)
-        gc = np.extract(rsums > r, rsums)
-        element = rsums.index(gc[0])
-        if (compounds['Compound'][element] not in seed_set):
-            seed_set.append(compounds['Compound'][element])
-            print(seed_set)
-
-    #VERSION: 5 random (with weights)
-    # while len(seed_set) < 5:
+    # while len(seed_set) < 10:
     #     r = random.uniform(0, np.sum(compounds['Concentration']))
+    #     #print(r)
     #     gc = np.extract(rsums > r, rsums)
     #     element = rsums.index(gc[0])
     #     if (compounds['Compound'][element] not in seed_set):
     #         seed_set.append(compounds['Compound'][element])
 
+
+    #VERSION: 5 random (with weights)
+    while len(seed_set) < 5:
+        r = random.uniform(0, np.sum(compounds['Concentration']))
+        gc = np.extract(rsums > r, rsums)
+        element = rsums.index(gc[0])
+        if (compounds['Compound'][element] not in seed_set):
+            seed_set.append(compounds['Compound'][element])
+
     #VERSION: 5 random (no weight)
-    # r = compounds.sample(n=5)
-    # seed_set = r['Compound'].tolist()
+    # seed_set = random.sample(compounds['Compound'].tolist(), 5)
     # print(seed_set)
 
     #VERSION: 10 random (no weight)
-    # r = compounds.sample(n=10)
-    # seed_set = r['Compound'].tolist()
+    # seed_set = random.sample(compounds['Compound'].tolist(), 10)
     # print(seed_set)
 
     #Final seet set outputting to a file
